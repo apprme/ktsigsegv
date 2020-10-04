253271736ee35cfe45ec74bad73dc9f7c1e6e331
Pavel Kakolin <appr@appr.me>
2020-10-04 05:56:57 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("multiplatform") version "1.4.20-M1"
+}
+
+group = "me.appr"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
+
+kotlin {
+    val nativeTarget = linuxArm64("native")
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+    sourceSets {
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
---
+++ src/nativeMain/kotlin/main.kt
@@ -0,0 +1,3 @@
+fun main() {
+  error("Hello, Kotlin/Native!")
+}
